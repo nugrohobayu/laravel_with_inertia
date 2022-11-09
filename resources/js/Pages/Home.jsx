10b596117dd2c55d94953f709014c4fdd2ecd4f0
Bayu Nugroho <nugrohob825@gmail.com>
2022-11-09 17:31:40 +0700

    fisrt commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Lorem .</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Laborum repudiandae quibusdam ipsam in!</p>
+    </div>
+  )
+}
